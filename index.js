f5cb40a9d4e1d0242372adcd6c681d2827e7abfc
jim1_lin <jim1_lin@asus.com>
2016-02-26 17:55:14 +0800

    kaiseki.sh start api-server with kaiseki-env-setting

--- index.js
+++ index.js
@@ -14,8 +14,9 @@ var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337'  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',
+  restAPIKey: process.env.REST_API_KEY || 'myRestAPIKey',
+  masterKey: process.env.MASTER_KEY || 'myMasterKey' //Add your master key here. Keep it secret!
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
---
+++ kaiseki.sh
@@ -0,0 +1,8 @@
+export VERBOSE=1
+export DATABASE_URI="mongodb://kaiseki:uoVDvekxzKVrPay12PR9rFhMk18xSiXjUFJRQ893@ds015928.mongolab.com:15928/shareddb"
+export APP_ID="uoVDvekxzKVrPay12PR9rFhMk18xSiXjUFJRQ893"
+export MASTER_KEY="bluFBl60CLGzETiJwR9waTopacxz9X6KXAuuPEnY"
+export REST_API_KEY="KaDTOFik6e2Zk94wVKkyNxahWCHdL9muzOFLoqr7"
+export SERVER_URL="http://localhost:1337"
+export PORT=1337
+npm start
